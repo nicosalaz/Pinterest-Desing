40b8120c0f1020d66470395d20bff24e476cea46
nicosalaz <71720590+nicosalaz@users.noreply.github.com>
2021-11-02 18:34:04 -0500

    Delete src directory

--- src/components/main.js
+++
@@ -1,5 +0,0 @@
-import PinLayout from './Pinterest-Layout/Pin-Layout/PinLayout';
-import Header from './headerPinterest/Header';
-import Footerfloat from './footerPiterest/Footerfloat';
-
-export { PinLayout,Header,Footerfloat };
