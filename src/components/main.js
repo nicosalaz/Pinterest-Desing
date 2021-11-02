5a1ec008e98a915d14eebfa886720a2aeb9248f5
nicosalaz <71720590+nicosalaz@users.noreply.github.com>
2021-11-02 10:39:06 -0500

    Add files via upload

---
+++ src/components/main.js
@@ -0,0 +1,5 @@
+import PinLayout from './Pinterest-Layout/Pin-Layout/PinLayout';
+import Header from './headerPinterest/Header';
+import Footerfloat from './footerPiterest/Footerfloat';
+
+export { PinLayout,Header,Footerfloat };
